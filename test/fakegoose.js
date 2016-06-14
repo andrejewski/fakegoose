c6362640cc0360be7c26a18bcbb3b460c29b7966
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-06-14 18:36:23 -0400

    added test file

---
+++ test/fakegoose.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+const fakegoose = require('../fakegoose');
+
+describe('fakegoose', () => {
+  describe('fake(...[, callback:(error, results)])', () => {
+    it('does not have any tests yet', () => {});
+  });
+
+  describe('fakeOne(...[, callback:(error, result)])', () => {
+    it('does not have any tests yet', () => {});
+  });
+
+  describe('seed(count[, forceAppend=false], callback:(error))', () => {
+    it('does not have any tests yet', () => {});
+  });
+});
+
